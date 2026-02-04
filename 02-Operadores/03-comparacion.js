82aeaf178250a43a41ed70118ddb3d8c5b70bbb9
Carlos Echagarreta <carlos.echagarreta@vml.com>
2026-02-04 08:59:57 -0300

    Actualizar

--- 02-Operadores/03-comparacion.js
+++ 02-Operadores/03-comparacion.js
@@ -10,5 +10,8 @@ console.log(a <= 5)
 console.log( a == 10);
 // distinto!
 console.log( a != 10);
-
+//+++++++++++++++++++++++++++++++++++++++++ Utilizar los siguentes tipos de datos : 
+// Preguntar por el valor y por el tipo de dato
 console.log( a ==="10");
+// distinto tipos de datos o valor
+console.log( a !=="10");
---
+++ 02-Operadores/04-logicos.js
@@ -0,0 +1,15 @@
+// AND, OR, NOT
+
+// AN && evalua lo que tiene a su izquiera y a su derecha 
+
+let mayor = false;
+let suscrito = false;
+
+// console.log( true && true);
+// console.log( false && true);
+
+console.log('operador AND',  mayor && suscrito)
+
+// operador logico OR evalua el valor de true si una de las condiciones se cumple, la unica manera que sea  de false es que las dos condiciones sea false = OR ||
+
+console.log('operador AND',  mayor || suscrito)
